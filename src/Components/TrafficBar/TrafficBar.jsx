873d6031e2785d01bc7b8a8f3ca43147f04950c4
Murtaza Abidi <murtaza2476175@gmail.com>
2023-01-11 22:37:06 +0500

    More work in Landing page

---
+++ src/Components/TrafficBar/TrafficBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './TrafficBarStyles.css'
+import Country1 from '../../assets/CountryFlags/Country1.svg'
+
+const TrafficBar = ({flag, traffic}) => {
+  return (
+    <div className='trafficBar-container'>
+        <img src={flag} alt="flag" />
+        <div className='progress-bar' style={{width:`${traffic}px`}}>
+            <div className='progress-bar-stats'>
+                <div className='progress-bar-stats-inner-div'>{traffic}</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default TrafficBar
--- src/Pages/Landing Page/index.jsx
+++ src/Pages/Landing Page/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import AboutUs from '../../Components/AboutUs'
 import Intro from '../../Components/Intro'
+import Traffic from '../../Components/Traffic'
 
 const LandingPage = () => {
     return (
         <div style={{ marginTop: '7rem' }}>
             <Intro />
+            <AboutUs/>
+            <Traffic/>
         </div>
     )
 }
