66eea9adbc7340fa90904b998eec930351786d7c
Murtaza Abidi <murtaza2476175@gmail.com>
2023-01-15 00:16:42 +0500

    Problem Facing by Society (Done) �

--- src/Components/ProblemsFacing/index.jsx
+++ src/Components/ProblemsFacing/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import ProblemFacingSlider from './ProblemFacingSlider'
+import bg from '../../assets/Problem_facing_background.svg'
 
 const ProblemFacing = () => {
   return (
     <div>
-        
+        <div className='problemfacing-backgroundImage'>
+        <img src={bg} alt="Problemfacing-Pic" />
+        </div>
+        <h1 className='ProblemFacing-heading'>Problems Facing by Society</h1>
+        <ProblemFacingSlider/>
     </div>
   )
 }
