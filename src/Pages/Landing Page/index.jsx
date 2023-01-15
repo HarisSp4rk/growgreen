b107afdc6368f4f26efa6f721fed44bca8ce1e96
Murtaza Abidi <murtaza2476175@gmail.com>
2023-01-15 13:09:51 +0500

    Have any Query ( Component Done ) !!

--- src/Pages/Landing Page/index.jsx
+++ src/Pages/Landing Page/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import AboutUs from '../../Components/AboutUs'
+import DevicesPlaced from '../../Components/DevicesPlaced'
+import HaveAnyQuery from '../../Components/HaveAnyQuery'
 import Intro from '../../Components/Intro'
 import ProblemFacing from '../../Components/ProblemsFacing'
 import Traffic from '../../Components/Traffic'
@@ -14,6 +16,8 @@ const LandingPage = () => {
             <AboutUs/>
             <Traffic/>
             <ProblemFacing/>
+            <DevicesPlaced/>
+            <HaveAnyQuery/>
             </div>
         </div>
     )
